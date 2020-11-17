2a73972e623f8d7c449ae246dc8421660c07658c
Exokem <ex010@live.com>
2020-11-16 23:30:12 -0500

    QueensChamber implementation

--- src/world/QueensChamber.java
+++ src/world/QueensChamber.java
@@ -1,5 +1,12 @@
 package world;
 
+import bee.Bee;
+import bee.Drone;
+import bee.Queen;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * The queen's chamber is where the mating ritual between the queen and her
  * drones is conducted.  The drones will enter the chamber in order.
@@ -9,8 +16,56 @@ package world;
  * for exiting (like with the worker bees and the flower field).
  *
  * @author Sean Strout @ RIT CS
- * @author YOUR NAME HERE
+ * @author Samuel Henderson
  */
 public class QueensChamber {
-    // TODO
+    private final ConcurrentLinkedDeque<Drone> drones = new ConcurrentLinkedDeque<>();
+
+    private boolean queenMating = false;
+
+    public synchronized void dismissDrone() {
+
+    }
+
+    public synchronized void enterChamber(Drone drone) {
+        // Add the provided drone to the chamber
+        System.out.println("*QC* " + drone + " enters chamber");
+        this.drones.add(drone);
+
+        if(!this.drones.getFirst().equals(drone) || this.queenMating) {
+            // Wait if the queen is already mating with another drone
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("*QC* " + drone + " leaves chamber");
+    }
+
+    /**
+     * Expresses the presence of any drones in the queen chamber according to the queue of drones that have entered.
+     */
+    public synchronized boolean hasDrone() {
+        // There is at least one drone if the queue is not empty
+        return !this.drones.isEmpty();
+    }
+
+    public synchronized void summonDrone() {
+        // Update status so other drones know
+        this.queenMating = true;
+
+        // Get the first drone in the queue
+        Drone first = this.drones.getFirst();
+        this.drones.remove(first);
+
+        // Cause drone to mate with queen, update drone accordingly
+        System.out.println("*QC* Queen mates with " + first);
+        first.mate();
+
+        // Update status and notify waiting drones
+        this.queenMating = false;
+        notifyAll();
+    }
 }
